b4edd373256ab62714bc1f5445d79f2205181936
dev-pbg <87623702+dev-pbg@users.noreply.github.com>
2023-10-22 16:59:54 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9e13ebcc400f466485a3e469546f71b6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Prithvi',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Prithvi Bharadwaj',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Personal website',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  twitter: 'maybeprithvi',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'prithvibharadwaj',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
